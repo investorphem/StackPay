8b5500b34883f7c487202b403e4ffad5868bf269
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-23 14:05:07 +0100

    Update CreateStream.jsx

--- components/CreateStream.jsx
+++ components/CreateStream.jsx
@@ -4,7 +4,7 @@ import { openContractCall } from "@stacks/connect";
 import { uintCV, standardPrincipalCV } from "@stacks
 import { contractAddress,contractName } from "../lib/contract";
 
-exp
+ex
   async function create() {
     await openContractCall({
       contractAddress,
