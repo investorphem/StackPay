683bd2f308124c42be2cf714cf3c0bdb1adedccb
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-23 13:35:04 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import CreateStream from "../../components/CreateStream";
 import StreamCard from "../../components/StreamCard";
-import { fetchStreams } from "../../lib/contract"; // uses
+import { fetchStreams } from "../../lib/contract"; // use
 
 export default function Dashboard() {
   const [streams, setStreams] = useState([]);
