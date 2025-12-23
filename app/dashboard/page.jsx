b5f4652f6e57d89baab27b98ed634a95400058ab
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-23 13:45:18 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -23,7 +23,7 @@ export default function Dashboard() {
       {/* Employer c*/}
       <CreateStream />
 
-      {/* Display all active*/}
+      {/* Display all activ*/}
       <div className="mt-6">
         {streams.length === 0 ? (
           <p>No active streams yet</p>
