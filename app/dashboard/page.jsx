b6540fd050978924017de1f9a5008307781fb747
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-23 13:46:05 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -26,7 +26,7 @@ export default function Dashboard() {
       {/* Display all activ*/}
       <div className="mt-6">
         {streams.length === 0 ? (
-          <p>No active streams l</p>
+          <p>No active streams </p>
         ) : (
           streams.map((stream) => (
             <StreamCard key={stream.id} stream={stream} />
