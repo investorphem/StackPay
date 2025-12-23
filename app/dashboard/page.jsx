9bc33dddb2a6a2555ebac1a95b7e491f2776c4b7
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-23 13:39:50 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import CreateStream from "../../components/CreateStream";
 import StreamCard from "../../components/StreamCard";
-import { fetchStreams } from "../../lib/contract"; /
+import { fetchStreams } from "../../lib/contract"; // uses get-all-streams
 
 export default function Dashboard() {
   const [streams, setStreams] = useState([]);
@@ -20,7 +20,7 @@ export default function Dashboard() {
     <div className="p-10">
       <h2 className="text-2xl font-bold mb-4">Dashboard</h2>
 
-      {/* Employer creates new stream */}
+      {/* Employer creates new streams */}
       <CreateStream />
 
       {/* Display all active streams */}
