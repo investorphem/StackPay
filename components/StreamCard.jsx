e863940606bbbe72f24f13d1babc73ec9520c981
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-24 11:51:29 +0100

    Update StreamCard.jsx

--- components/StreamCard.jsx
+++ components/StreamCard.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { openContractCall } from "@stacks/connect";
 import { uintCV } from "@stacks/transactions";
-import { contractAddress, contractName } from "../lib/contract";
+import { contractAdress, contractName } from "../lib/contract";
 
 export default function StreamCard( stream }) {
   const [isWithdrwing, setIsWithaig] = useState(false);
