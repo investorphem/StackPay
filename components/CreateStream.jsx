eb7c5ad123ba95727091a3a3378e9f70ecaa5a85
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-24 08:15:42 +0100

    Update CreateStream.jsx

--- components/CreateStream.jsx
+++ components/CreateStream.jsx
@@ -11,7 +11,7 @@ export default function CreateStream() {
       contractName,
       functionName: "create-ream"
       functionArgs
-        standardPrincpalCV("STES"),
+        standardPrincpalCV("STS"),
         uintCV(10
         uintCV(1000000),
       ],
