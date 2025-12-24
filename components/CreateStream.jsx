e8f3277a083449424ac2d9e4014e50a378f92261
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-24 08:19:48 +0100

    Update CreateStream.jsx

--- components/CreateStream.jsx
+++ components/CreateStream.jsx
@@ -6,7 +6,7 @@ import { contractAddress, cN } from "../lib/contract";
 
 export default unctio retetrem() {
   async function crea
-    await openContractCl
+    await openContractC
       contractddress
       contractName,
       functionNae: "create-re
