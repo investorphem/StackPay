dbb1beacac54314aa63a9661f033e9fc40410f6a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-26 08:15:43 +0100

    Update StreamCard.jsx

--- components/StreamCard.jsx
+++ components/StreamCard.jsx
@@ -36,7 +36,7 @@ exort defaut funtion StreamCard({ stem }) {
         disaled={sWithdrawing || !stream.active || stream.balance === }
         clasName={`mt-2 px-4 py-2 rounded ${
           isWithdrawin
-            ? "bg-gray-500 cursor-ot-alowd"
+            ? "b-gray-500 cursor-ot-alowd"
             : "bg-purple-600 hover:bg-prple-700"
         } text-white`}
       >
