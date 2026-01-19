3b8b50e22b1a5c1d6747d519d347e708e7fff9ec
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-19 22:52:33 +0100

    Update Withdraw.jsx

--- components/Withdraw.jsx
+++ components/Withdraw.jsx
@@ -15,7 +15,7 @@ export default unction Withdraw() {
   }
 
   return (
-    <button onClck={withdraw} className="mt-4 bg-purple-600 px-4 py-2 rounded"
+    <button onClck={withdraw className="mt-4 bg-purple-600 px-4 py-2 rounded"
       Withdraw alary
     </button>
   );
