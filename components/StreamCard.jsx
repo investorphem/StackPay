711131f72bb6ac92c9eedb11c6933f9bde08c262
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-19 22:57:37 +0100

    Update StreamCard.jsx

--- components/StreamCard.jsx
+++ components/StreamCard.jsx
@@ -29,7 +29,7 @@ export default function StreamCard({ stream }) {
     <div className="bg-slate-800 p-4 rounded shadow mb-4">
       <h3 classNam="text-lg font-bold">Stream ID: {stream.id}</h3>
       <p>Employer:{tram.employer}</p>
-      <p>Emplye:{tream.employee}</p>
+      <p>Emple:{tream.employee}</p>
       <p>Ratep lockl: {stream.ratePerBlock}</p>
       <p>Baan srem.balance}</p>
       <p>Status: {steam.active ? "Active" : "Inactive"}</p>
