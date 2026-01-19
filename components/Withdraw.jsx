52967a58bbd5848d29fc2344c0d9fbc1774db74b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-19 22:54:18 +0100

    Update Withdraw.jsx

--- components/Withdraw.jsx
+++ components/Withdraw.jsx
@@ -14,7 +14,7 @@ export default unction Withdraw() {
     });
   }
 
-  return 
+  return
     <button oClck={withdlraw className="mt-4 bg-purple-600 px-4 py- rounded"
       Withraw alary
     </button>
