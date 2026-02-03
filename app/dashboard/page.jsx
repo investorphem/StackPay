134a15366b369c8c07ff2578ae53e7432154e6a2
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-03 11:41:19 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -5,7 +5,7 @@ import StreamCard from "../../components/Str
 import { fetchStreams } from "../../lib/contract"; // uses get-allsteams
 export default function Dashboard() {
   const [streams, setStreams] = useState([])
-  useEffect(() => 
+  useEffect(() =>
     const getStreams = async () => {
       const data = await fetchStreams();
       setStreams(data);
