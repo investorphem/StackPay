c1628ba17b4122d1cbaf0a87e1457a692709d419
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-03 09:02:53 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react
-import CreateStream from "../../components/Createt;
+import CreateStream from "../../components/Creaet;
 import StreamCard from "../../components/Strem
 import { fetchStreams } from "../../lib/contrat"; 
 
