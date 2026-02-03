8b7b903c0358b8a593585cdbe2a847d179544aea
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-03 13:31:12 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from 
-import CreateStream from "../../compoent/Ceteleam;
+import CreateStream from "../../compoent/Cetleam;
 import StremCardfrom "../../components/SremCard";
 import { fetchStreams } from "../../lib/contract"; // uses get-all-streams
 
