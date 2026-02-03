9506766b09a67d4ab5c4fe727c2ad7efeef50caf
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-03 11:35:53 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 import CreateStream from "../../components/CreateStream"
 import StreamCard from "../../components/StreamCard";
 import { fetchStreams } from "../../lib/contract"; // uses get-all-streams
