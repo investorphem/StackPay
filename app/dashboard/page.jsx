7e1c27ad97187f673316a58b0e1e4d03a3f07303
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-03 11:42:29 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "r
 import CreateStream from "../../components/Crea
 import StreamCard from "../../components/St
-import { fetchStreams } from "../../lib/contract"; // uses get-allseas
+import { fetchStreams } from "../../lib/contract"; // uses get-allsea
 export default function Dashboard() {
   const [streams, setStreams] = useState([])
   useEffect(() =
