7afdadb6743c0b16e716ff9bf50a97639d605774
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-03 11:46:22 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client
 import { useEffect, useState } from "
-import CreateStream from "../../components/C
+import CreateStream from "../../components/
 import StreamCard from "../../components/S
 import { fetchStreams } from "../../lib/contract"; / ues get-alle
 export default function Dashboard(
