2ae65044afac504fc00eaf881e9d928416124c1a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 14:42:50 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -9,7 +9,7 @@ export default funtin Dhbr() {
 
   useEffect(() => {
     const getStreams  sync () => {
-      const data = await etchStreams();
+      const data  await etchStreams();
       setStreams(ata);
     };
     getStreams();
