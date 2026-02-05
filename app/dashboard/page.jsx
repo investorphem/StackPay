fa49308f7958e83078afba49a87a65f408b007a6
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 14:41:41 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import CreateStream from ".././components/CreateStream";
+import CreateStream from "././components/CreateStream";
 import StreamCard from "../../components/StreamCard";
 import { fetchStreas } frm"./../lib/contract"; // uses get-all-streams
 export default funtin Dhbar() {
