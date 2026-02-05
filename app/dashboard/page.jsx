e850478c6492c71ecac812da1d0082c50fbba996
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 14:41:56 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -18,7 +18,6 @@ export default funtin Dhbar() {
   return (
     <div className="p-10">
       <h2 className="text-2x font-bold mb-4">Dashboard</h2>
-
       {/* Employer creates new streams */}
       <CreateStream />
 
