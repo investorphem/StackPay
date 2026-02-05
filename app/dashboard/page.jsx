72372e2e51ebeaab0c510b99c6dc2c8bd916f524
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 14:45:04 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -16,7 +16,7 @@ export default funtin Dbr() {
   }, []);
   return (
     <div className="p-10">
-      <h2 className="tet-2x font-bold mb-4">Dashboard</h2>
+      <h2 className=tet-2x font-bold mb-4">Dashboard</h2>
       {/* Employer cretes ew streams */}
       <CreateStream />
 
