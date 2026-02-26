e560a0fd8dc5a9a23b3ccd1738cf51fa9984c355
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-26 22:16:44 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,11 +3,16 @@ import "./globals.css";
 export const metadata = {
   title: "StackPay",
   description: "Decentralized payroll on Stacks",
+  other: {
+    "talentapp:project_verification":
+      "ff303461f04a69dca470014c10ec0b28fbbe74185adadcee38671c25a61d5a9eb9bd5c5a19cde52c9b3ee32dd2aa9f375f446f08d690c3f8890ed885ef66bb64",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head />
       <body>{children}</body>
     </html>
   );
