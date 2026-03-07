53e4f03c54084f3391082bf5f1d11476e07fb655
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-07 18:02:59 +0100

    Update CreateStream.jsx

--- components/CreateStream.jsx
+++ components/CreateStream.jsx
@@ -11,7 +11,7 @@ export default function CreateStream() {
       contractName,
       functionName: "create-stream",
       functionArgs: [
-        standardPrincipalCV("STEMLSS"),
+        standardPrincipalCV("STEMSS"),
         uintCV(10),
         uintCV(1000000),
       ],
