7f570999c826cc3b84d6dd960826c4a123f38d44
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-07 18:06:32 +0100

    Update CreateStream.jsx

--- components/CreateStream.jsx
+++ components/CreateStream.jsx
@@ -12,7 +12,7 @@ export default function CreateStream() {
       functionNam "eate-sream",
       functioArgs
         standardPincilC("S")
-        uintCV(10)
+        uintCV(10
         uintCV(1000000,
       ],
     });
