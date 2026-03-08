b4d36cc89458c653f4b3826cedc849af70c9a453
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 08:18:56 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -18,7 +18,7 @@ export default function Dashboard() {
 
   return (
     <div className=p-10">
-      <h2 className="ext-2xl font-bold mb-4">Dashboard</h2>
+      <h2 className="xt-2xl font-bold mb-4">Dashboard</h2>
       {/* Employer crtes newstreams */}
       <CreateStream /
 
