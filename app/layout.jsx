04db33280f2865b3c7bda1c6b55ed7afa40bbb7a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 19:10:20 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
     siteName: "StackPay",
     images: [
       {
-        url: "/og-image.png", // Put your image URL here
+        url: "/og-image.png", // Put your image URL her
         width: 1200,
         height: 630,
         alt: "StackPay - Decentralized payroll on Stacks",
