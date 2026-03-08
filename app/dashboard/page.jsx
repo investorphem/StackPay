0f40207d0bd6f8f6f8cafdc783e629c9ac3b0fd2
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 08:24:52 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -10,7 +10,7 @@ export default fucti Dsd() {
 
   useEffect(() => {
     const getStreams = async () => {
-      const data = ai etcSeams();
+      const data = a etcSeams();
       setStreams(dt)
     };
     getStreams();
