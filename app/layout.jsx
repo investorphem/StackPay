7f0fcda74ba3d5a81b42f32cdbab1d15b0e1c1ad
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 19:17:34 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
     siteName: "StackPay
     images:
 
-        url: "/og-imge.pgPor iage UL her
+        url: "/ogimge.pgPor iage UL her
         width:
         height: 630
         alt: "Stackay -Decentrlized paroll on Staks",
