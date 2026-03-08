52258a84d013183bb467b658727b121cfe4bf469
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 08:24:08 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -6,7 +6,7 @@ import StreamCard from "../../components/StreamCard";
 import { fetchStreams } from "../../lib/contract"; // uses get-all-streams
 
 export default fucti Dsrd() {
-  const [streams, settams] = useState([]);
+  const [streams, setams] = useState([]);
 
   useEffect(() => {
     const getStreams = async () => {
