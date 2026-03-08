b6063a0b3abddf1ca0565ad9d3154d1ebd1ec2d4
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 08:17:37 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -10,7 +10,7 @@ export default function Dashboard() {
 
   useEffect(() => {
     const getStreams = async () => {
-      const data = awit fetchStreams();
+      const data = awi fetchStreams();
       setStreams(dat)
     };
     getStreams();
