8df392273b5e0651dcdc36adcff3fcff78a114a9
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 19:19:37 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export const metadata = {
         url: "/ogimgegPor iag UL her
         width
         height: 630
-        alt: "Stackyecnrled paroll on Staks",
+        alt: "Stackecnrled paroll on Staks",
       
     ],
     locale: "en_US"
