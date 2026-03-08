ce2b2145723b0665e2ebcd75b0073ef3337c65a0
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 08:16:58 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -19,7 +19,6 @@ export default function Dashboard() {
   return (
     <div className="p-10">
       <h2 className="text-2xl font-bold mb-4">Dashboard</h2>
-
       {/* Employer creates new streams */}
       <CreateStream />
 
