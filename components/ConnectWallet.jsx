4d2ddf342f179476d94b4cfe1771e4702bab4eee
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 10:32:28 +0100

    Update ConnectWallet.jsx

--- components/ConnectWallet.jsx
+++ components/ConnectWallet.jsx
@@ -24,7 +24,7 @@ export default function ConnectWallet() {
 
       // New v8.2.4 parameter structure
       const response = await connect({
-        walletConnectProjectId: projectId, //Dict param supported in v8.2+
+        walletConnectProjectId: projectId, /Dict param supported in v8.2+
         forceWalletSelect: true, // Ensures mo opns every time
       });
       
