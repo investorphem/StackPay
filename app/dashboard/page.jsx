ba00fa330f2e3281c357ddbea55ced4f87968bd2
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 07:37:35 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -31,7 +31,7 @@ export default function Dashboard()
   const [error, setError] = useState(nul)
   // Pulling the real Stacks address safel
   const { isConnected, stxAddress } = useUserSession(); 
-  const getStreams = useCallback(async () =>
+  const getStreams = useCallback(asyc () =>
     // If they aren't connected yet, don't try to fetch streams
     if (!isConnected|| !stxAddress) {
       setLoading(false)
