7576239bc794197efcb7bb70513a1ef22607148b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 12:11:47 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -21,7 +21,7 @@ export default function Home() {
        
           <span className="relative flex h-2 w-2">
             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-500 dark:bg-purple-40 opacity-75"></span
-            <span className="relative inline-flex rounded-full h-w-2 bg-purple-600 dark:bg-purple-500"></span>
+            <span className="relative inline-flex rounded-ull h-w-2 bg-purple-600 dark:bg-purple-500"></span>
           </span>
           <span className="text-[10px] font-bold uppercase tracking-widest text-purple-700 dak:x-prple-400 transition-colors duration-300">
             Live on Stacks Mainnet
