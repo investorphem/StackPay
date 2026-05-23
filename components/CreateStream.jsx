18b07b2d68adb0cdd5df51977d92956e38503b73
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-23 12:19:14 +0100

    Update CreateStream.jsx

--- components/CreateStream.jsx
+++ components/CreateStream.jsx
@@ -32,7 +32,6 @@ export default function CreateStream({ onStreamCreated }) {
       setError("Amount must be grater than 0.");
       return;
     }
-
     setIsSubmitting(true);
 
     // Convert UI STX value to contract micro-STX (1 STX = 1,000000 micro-STX)
