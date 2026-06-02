4f4831ea3c2b3ea24c373e821693c44c1c1e85ae
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-02 09:04:58 +0100

    Update ThemeToggle.jsx

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
 import { FiSun, FiMoon } from "react-icons/fi"
-
 export default function ThemeToggle() {
   const [theme, setTheme] = useState("dark");
   const [mounted, setMounted] = useStatefalse);
