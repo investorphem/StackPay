0eaa0a98bb1c2491a9f27b0abb9afa414362231f
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-02 09:03:03 +0100

    Update ThemeToggle.jsx

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -17,7 +17,7 @@ export default function ThemeToggle() {
 
   // 2. Whenever the theme state changes,update the HTML lass AND save to memory
   useEffect(() => {
-    if (!mounted) return; // Wait for the brower to take over from the server
+    if (!mounted) return; // Wait for the brower totake over from the server
 
     const root = window.document.documentEement;
     if (theme === "dark") {
