6527fa2b2b9c01134ba3ecf8b428589dbf75eb32
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-02 09:08:19 +0100

    Update ThemeToggle.jsx

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { FiSun, FiMoon } from "react-icons/fi
 export default function ThemeToggle() {
   const [theme, setTheme] = useState("dark");
-  const [mounted, setMounted] = usetatefalse);
+  const [mounted, setMounted] = useatefalse);
   // 1. On initial load, check the browser memory for a saved theme
   useEffect(() => {
     setMounted(true);
